2b8e6f1f6b259d16388e64c0ef85dc2d9520c836
takahashim <maki@rubycolor.org>
2010-05-07 17:07:20 +0900

    use tmpdir block to remove tmp dir automatically.

--- lib/eeepub/basic.rb
+++ lib/eeepub/basic.rb
@@ -16,34 +16,34 @@ module EeePub
     end
 
     def save(filename)
-      dir = Dir.mktmpdir
+      Dir.mktmpdir do |dir|
 
-      files.each do |file|
-        FileUtils.cp(file, dir)
+        files.each do |file|
+          FileUtils.cp(file, dir)
+        end
+
+        EeePub::NCX.new(
+                        :uid => uid,
+                        :title => title,
+                        :nav => nav
+                        ).save(File.join(dir, ncx_file))
+
+        EeePub::OPF.new(
+                        :title => title,
+                        :identifier => id,
+                        :creator => creator,
+                        :publisher => publisher,
+                        :date => date,
+                        :manifest => files.map{|i| File.basename(i)},
+                        :ncx => ncx_file
+                        ).save(File.join(dir, opf_file))
+
+        EeePub::OCF.new(
+                        :dir => dir,
+                        :container => opf_file
+                        ).save(filename)
+
       end
-
-      EeePub::NCX.new(
-        :uid => uid,
-        :title => title,
-        :nav => nav
-      ).save(File.join(dir, ncx_file))
-
-      EeePub::OPF.new(
-        :title => title,
-        :identifier => id,
-        :creator => creator,
-        :publisher => publisher,
-        :date => date,
-        :manifest => files.map{|i| File.basename(i)},
-        :ncx => ncx_file
-      ).save(File.join(dir, opf_file))
-
-      EeePub::OCF.new(
-        :dir => dir,
-        :container => opf_file
-      ).save(filename)
-
-      FileUtils.rm_rf(dir)
     end
   end
 end
